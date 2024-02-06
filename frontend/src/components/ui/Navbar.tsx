66d3cb42b315122a9ef9d3465495a1a33e191d81
abematt <abrahammathew.dev@gmail.com>
2024-02-05 20:27:24 -0600

    Fix for Navbar import issue

--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import { useLogOut } from "@/hooks/useLogout";
-import { useAuthContext } from "@/hooks/useAuthContext";
+import { useLogOut } from "../../hooks/useLogOut";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function Navbar() {
   const { logout } = useLogOut();
